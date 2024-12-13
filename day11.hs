6af292134ab6097b0c48645439b616d9c95ab6f9
Chrysanthemum Lovelace <chryslovelace@gmail.com>
2024-12-13 01:01:13 -0500

    day 11 fast mode

--- day11.hs
+++ day11.hs
@@ -1,18 +1,28 @@
 import System.IO
 import Numeric
+import Data.Map
 
-blink :: [Int] -> [Int]
-blink [] = []
-blink (0:xs) = 1 : blink xs
-blink (x:xs) = let len = 1 + floor (logBase 10 (fromIntegral x)) in
+blinkOne :: Int -> Either Int (Int, Int)
+blinkOne 0 = Left 1
+blinkOne x = let len = 1 + floor (logBase 10 (fromIntegral x)) in
     if even len then
-        div x (10 ^ div len 2) : rem x (10 ^ div len 2) : blink xs
+        Right (div x (10 ^ div len 2), rem x (10 ^ div len 2))
     else
-        x * 2024 : blink xs
+        Left $ x * 2024
+
+blinkMap :: Map Int Int -> Map Int Int
+blinkMap = foldlWithKey f empty where
+    f a k v = case blinkOne k of
+        Left n -> insertWith (+) n v a
+        Right (l, r) -> insertWith (+) l v $ insertWith (+) r v a
+
+countStones :: Map Int Int -> Int
+countStones = foldlWithKey f 0 where
+    f a _ v = a + v
 
 main = do
     inputFile <- readFile "inputs/day11.txt"
-    let stones = map read $ words inputFile
-    let blinks = iterate blink stones
-    print $ length $ blinks !! 25
-    print $ length $ blinks !! 75
+    let stones = fromList $ Prelude.map (\x-> (read x, 1)) $ words inputFile
+    let blinks = iterate blinkMap stones
+    print $ countStones $ blinks !! 25
+    print $ countStones $ blinks !! 75
